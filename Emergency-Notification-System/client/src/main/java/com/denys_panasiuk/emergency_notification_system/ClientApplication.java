40c10b4914e2a96798fd38acbccce09949f19f5a
DenisPanasiuk <114135209+DenisPanasiuk@users.noreply.github.com>
2023-05-27 20:06:22 +0200

    Created project structure and modules
    Created 3 modules:
    -client
    -distribute
    -server
    And added main dependencies

---
+++ Emergency-Notification-System/client/src/main/java/com/denys_panasiuk/emergency_notification_system/ClientApplication.java
@@ -0,0 +1,11 @@
+package com.denys_panasiuk.emergency_notification_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ClientApplication.class, args);
+    }
+}
